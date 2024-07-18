1635d86b02d039a4b9afe834a9b92d671643eec9
Grafscorp <golyandringraf@gmail.com>
2024-07-18 14:42:48 +0300

    Update Background Color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/ui_pages/city_change_page.dart';
 import 'package:weather_app/ui_pages/weather_page.dart';
-import 'package:weather_app/ui_pages/welcome_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,21 +12,22 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        title: "Weather App",
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          body: Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.bottomLeft,
-                    end: Alignment.topRight,
-                    colors: [
-                      Color.fromARGB(255, 59, 240, 247),
-                      Color.fromARGB(255, 12, 129, 224)
-                    ]),
-              ),
-              child: const WeatherPage(),
+          body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  colors: [
+                    Color.fromARGB(255, 164, 124, 181),
+                    Color.fromARGB(255, 235, 178, 205)
+                  ]),
             ),
+            child: //const CityChangePage(),
+
+                const WeatherPage(),
           ),
         ));
   }
