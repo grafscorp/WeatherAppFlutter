1517cfd08da5ed44ac190ea9ed2a767ff43efb32
Grafscorp <golyandringraf@gmail.com>
2024-07-18 14:43:08 +0300

    Add constants List

--- lib/static/constatns_theme.dart
+++ lib/static/constatns_theme.dart
@@ -1,3 +1,34 @@
 // ignore_for_file: constant_identifier_names
 
-const OPENWEATHER_API_KEY = "6310044bb9530078344174ec7128b1f5";
+const OPENWEATHER_API_KEY = "a66c202a7ef3e492b5c091eccf6250c3";
+
+const URL_WEATHER_ICONS = "http://openweathermap.org/img/wn/";
+
+const WEEKEDAYS = [
+  "Загрузка",
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+  "Воскресенье",
+];
+
+const MOUNTHS_NAME = [
+  "Загрузка",
+  "Января",
+  "Февраля",
+  "Апреля",
+  "Марта",
+  "Мая",
+  "Июня",
+  "Июля",
+  "Августа",
+  "Сентября",
+  "Октября",
+  "Ноября",
+  "Декабря",
+];
+
+const CITY_LIST = ["Воронеж", "Москва"];
